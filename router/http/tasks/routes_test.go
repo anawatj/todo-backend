7ffeeaaf12ff882506ee1f1d31d19fae0c2aacf3
anawatj <ajarusiripot@gmail.com>
2023-08-20 08:44:59 +0700

    mock delete success

--- router/http/tasks/routes_test.go
+++ router/http/tasks/routes_test.go
@@ -2,6 +2,7 @@ package tasks_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -132,6 +133,34 @@ func TestDeleteApi(t *testing.T) {
 		taskServiceMock.On("GetTaskByID", mock.AnythingOfType("string")).Return(&task, nil)
 
 		taskServiceMock.On("DeleteTask", mock.AnythingOfType("string")).Return(nil)
+		gin := gin.New()
+		rec := httptest.NewRecorder()
+		handler := tasks.TaskHandler{
+			Service: taskServiceMock,
+		}
+		gin.DELETE("/tasks/:id", handler.DeleteTask)
+		req := httptest.NewRequest(http.MethodDelete, `/tasks/4f19cbbc-8c2c-49dd-b48a-eabafb6ab7f2`, nil)
+		gin.ServeHTTP(rec, req)
+
+		t.Run("test status code and response body", func(t *testing.T) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+
+		})
+	})
+
+	t.Run("test delete error", func(t *testing.T) {
+		task := domains.Task{
+			ID:          "4f19cbbc-8c2c-49dd-b48a-eabafb6ab7f2",
+			Title:       "test",
+			Description: "test",
+			Status:      "IN_PROGRESS",
+			Image:       "MTExMQ==",
+			CreateAt:    time.Date(2023, time.August, 19, 22, 1, 46, 785911500, time.Local),
+		}
+		taskServiceMock := new(mocks.TaskServiceMock)
+		taskServiceMock.On("GetTaskByID", mock.AnythingOfType("string")).Return(&task, nil)
+
+		taskServiceMock.On("DeleteTask", mock.AnythingOfType("string")).Return(fmt.Errorf("Hello World"))
 		gin := gin.New()
 		rec := httptest.NewRecorder()
 		handler := tasks.TaskHandler{
